1ccfcce8838ac133a19cdfb57590db225ca7baae
meyyappan055 <meyyappan055@gmail.com>
2024-12-05 13:39:01 +0530

    feat: add cookies for session persistence

--- app/test_login.py
+++ app/test_login.py
@@ -1,10 +1,13 @@
 from playwright.sync_api import Playwright, sync_playwright
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
+
+COOKIES_FILE = "session_cookies.json"
 
 def login(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -22,7 +25,12 @@ def login(playwright: Playwright):
 
         iframe.get_by_placeholder("Enter Password").fill(PASSWORD)
         iframe.get_by_role("button", name="Sign In").click()
-        page.wait_for_timeout(5000) 
+        page.wait_for_timeout(5000)
+
+        cookies = context.cookies() #using cookies as site is limiting login requests while testing
+        with open(COOKIES_FILE, "w") as f:
+            json.dump(cookies, f)
+        print(f"Cookies saved to {COOKIES_FILE}")
 
     except Exception as e:
         print(f"Login error: {e}")
@@ -30,9 +38,7 @@ def login(playwright: Playwright):
         return None, None, None
 
     print("Login successful!")
-
     return page, context, browser
-
 
 if __name__ == "__main__":
     with sync_playwright() as playwright:
